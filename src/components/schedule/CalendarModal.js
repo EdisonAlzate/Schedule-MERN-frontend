72fa22852356237c8e0e186f1c5f84e8ce79e599
unknown <edisonalzate11@gmail.com>
2021-07-10 17:56:48 -0500

    update form modal

--- src/components/schedule/CalendarModal.js
+++ src/components/schedule/CalendarModal.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import Modal from 'react-modal';
+import DateTimePicker from 'react-datetime-picker';
+import moment from 'moment'
+
 
 
 const customStyles = {
@@ -14,25 +17,93 @@ const customStyles = {
 };
 Modal.setAppElement('#root');
 
+const now=moment().minutes(0).seconds(0).add(1,'hours')
+const nowPlus1=now.clone().add(1,'hours')
+
+
 export const CalendarModal = () => {
    
-const [isOpen, setIsOpen] = useState(true)
+const [dateStart, setDateStart] = useState(now.toDate())
+const [dateEnd, setDateEnd] = useState(nowPlus1.toDate())
 
     const closeModal=()=>{
-        setIsOpen(false)
+        
+    }
+
+    const handleStartDateChange=(e)=>{
+        setDateStart(e)
+    }
+
+    const handleEndDateChange=(e)=>{
+        setDateEnd(e)
     }
    
     return (
         <Modal
-          isOpen={ isOpen }
+          isOpen={ true }
           onRequestClose={ closeModal }
-          style={ customStyles }
-         /*  closeTimeoutMS={ 200 } */
+          style={ customStyles }  
+          
           className="modal"
           overlayClassName="modal-fondo"
           /* contentLabel="Modal example" */
           >
-          modal 
+          <h1> New Event </h1>
+          <hr />
+          <form className="container">
+
+    <div className="form-group">
+        <label>Date and hour start</label>
+        <DateTimePicker
+           onChange={handleStartDateChange}
+           value={dateStart}
+           className="form-control"
+        />
+    </div>
+
+    <div className="form-group">
+        <label>Date and hour end</label>
+        <DateTimePicker
+           onChange={handleEndDateChange}
+           value={dateEnd}
+           minDate={dateStart}
+           className="form-control"
+        />
+    </div>
+
+    <hr />
+    <div className="form-group">
+        <label>Title and notes</label>
+        <input 
+            type="text" 
+            className="form-control"
+            placeholder="Títle of event"
+            name="title"
+            autoComplete="off"
+        />
+        <small id="emailHelp" className="form-text text-muted">A short description</small>
+    </div>
+
+    <div className="form-group">
+        <textarea 
+            type="text" 
+            className="form-control"
+            placeholder="Notes"
+            rows="5"
+            name="notes"
+        ></textarea>
+        <small id="emailHelp" className="form-text text-muted">More information</small>
+    </div>
+
+    <button
+        type="submit"
+        className="btn btn-outline-primary btn-block"
+    >
+        <i className="far fa-save"></i>
+        <span> Save</span>
+    </button>
+
+</form>
           </Modal>
     )
 }
