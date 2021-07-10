84291a66c25f489506ece26cc52a799a5113fb35
unknown <edisonalzate11@gmail.com>
2021-07-10 11:30:17 -0500

    router done

--- src/ScheduleApp.js
+++ src/ScheduleApp.js
@@ -1,29 +1,10 @@
 import React from 'react'
 import { RouterApp } from './router/RouterApp'
 
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
- 
-} from "react-router-dom";
-import { ScreenLogin } from './components/authentication/ScreenLogin';
-
 export const ScheduleApp = () => {
-    return (
-    <Router>
-         <div>
-             
-            <Switch>
-                <Route exact path="/login" component={ScreenLogin}/>
-                <Route exact path="/" component={ScheduleApp}/>
-
-                <Redirect to ="/"/>
-            </Switch>
-
-         </div>
-    </Router>
-    )
+  return (
+    <div>
+    <RouterApp/>
+    </div>
+  )
 }
--- src/router/RouterApp.js
+++ src/router/RouterApp.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+ 
+} from "react-router-dom";
+import { ScreenLogin } from '../components/authentication/ScreenLogin';
+import { ScreenSchedule } from '../components/schedule/ScreenSchedule';
+
+
+
 export const RouterApp = () => {
     return (
-        <div>
-        RouterApp
-        </div>
+    <Router>
+         <div>
+             
+            <Switch>
+                <Route exact path="/login" component={ScreenLogin}/>
+                <Route exact path="/" component={ScreenSchedule}/>
+
+                <Redirect to ="/"/>
+            </Switch>
+
+         </div>
+    </Router>
     )
 }
