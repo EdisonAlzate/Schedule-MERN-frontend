9dfa2772fd631b1d023456ffbcf09b473a194b7f
unknown <edisonalzate11@gmail.com>
2021-07-10 14:39:48 -0500

    schedule added ui

--- src/components/schedule/ScreenSchedule.js
+++ src/components/schedule/ScreenSchedule.js
@@ -1,11 +1,50 @@
 import React from 'react'
 import { Navbar } from '../userExperience/Navbar'
 
+import {Calendar,momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+const localizer = momentLocalizer(moment)
+
+const events=[{
+  title:'I Starting in my new job',
+  start:moment().toDate(),
+  end:moment().add(2,'hours').toDate(),
+  bgcolor:'#fafa',
+  notes:'reto'
+}]
+
+
+
 export const ScreenSchedule = () => {
+
+    const eventStyleGetter=(event, start, end, isSelected)=>{
+      console.log(event, start, end, isSelected)
+      const style={
+        backgroundColor:'green',
+        borderRadius:'0px',
+        opacity:0.8,
+        display:'block',
+        color:'white'
+      }
+
+      return{
+        style
+      }
+    }
+
     return (
-        <>
+        <div className="schedule-screen">
         
           <Navbar/> 
-        </>
+
+          <Calendar
+             localizer={localizer}
+             events={events}
+             startAccesor="start"
+             endAccessor="end" 
+             eventPropGetter={eventStyleGetter}  
+          />
+        </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ScheduleApp } from './ScheduleApp';
 
+import './styles.css'
 
 ReactDOM.render(
   
