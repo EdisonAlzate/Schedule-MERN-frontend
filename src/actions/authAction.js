8cd3b4b1010b41ee0cc52036823c2908dae31e95
unknown <edisonalzate11@gmail.com>
2021-07-15 15:10:19 -0500

    logout done

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -78,3 +78,18 @@ const login =(user)=>({
 
   }  
 }
+
+
+export const startLogout=()=>{
+    return(dispatch)=>{
+
+        localStorage.clear()
+        dispatch(logout());
+
+    }
+}
+
+const logout=()=>({
+    type:types.authLogout
+
+})
