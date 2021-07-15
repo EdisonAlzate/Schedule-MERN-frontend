ba7099a13082a01df366917e74ae34adbea878c6
unknown <edisonalzate11@gmail.com>
2021-07-15 14:48:27 -0500

    protection of routes

--- src/router/RouterApp.js
+++ src/router/RouterApp.js
@@ -1,35 +1,54 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
   Redirect
  
 } from "react-router-dom";
 import { startChecking } from '../actions/authAction';
 import { ScreenLogin } from '../components/authentication/ScreenLogin';
 import { ScreenSchedule } from '../components/schedule/ScreenSchedule';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 
 
 export const RouterApp = () => {
 
     const dispatch = useDispatch()
+    const {checking,uid} = useSelector(state => state.auth)
 
     useEffect(() => {
       dispatch(startChecking());
     }, [dispatch])
+
+    console.log(checking)
+    if(checking){
+      return <h5>Wait...</h5>;
+    }
 
     return (
     <Router>
          <div>
              
             <Switch>
-                <Route exact path="/login" component={ScreenLogin}/>
-                <Route exact path="/" component={ScreenSchedule}/>
+                <PublicRoute
+                 exact
+                 path="/login" 
+                 component={ScreenLogin}
+                 isAuthenticated={!!uid}
+                 />
+
+                <PrivateRoute
+                 exact 
+                 path="/" 
+                 component={ScreenSchedule}
+                 isAuthenticated={!!uid}
+                 
+                 />
 
                 <Redirect to ="/"/>
             </Switch>
